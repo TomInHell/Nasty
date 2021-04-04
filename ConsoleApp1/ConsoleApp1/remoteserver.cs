0022d774917a6b04e82533befc74222bcf6198b3
TomInHell <tamilat2002@gmail.com>
2021-04-04 17:12:40 +0300

    Твои мысли пахнут совсем не так, как слова. И это слышно.

---
+++ ConsoleApp1/ConsoleApp1/remoteserver.cs
@@ -0,0 +1,60 @@
+﻿using sun.rmi.transport.tcp;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Remoting;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class remoteserver
+    {
+        static void Main(string[] args)
+        {
+            // Инициализация
+            IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+            int port = 8888;
+            TcpListener server = new TcpListener(localAddr, port);
+            // Запуск в работу
+            server.Start();
+            // Бесконечный цикл
+            while (true)
+            {
+                try
+                {
+                    // Подключение клиента
+                    TcpClient client = server.AcceptTcpClient();
+                    NetworkStream stream = client.GetStream();
+                    // Обмен данными
+                    try
+                    {
+                        if (stream.CanRead)
+                        {
+                            byte[] myReadBuffer = new byte[1024];
+                            StringBuilder myCompleteMessage = new StringBuilder();
+                            int numberOfBytesRead = 0;
+                            do
+                            {
+                                numberOfBytesRead = stream.Read(myReadBuffer, 0, myReadBuffer.Length);
+                                myCompleteMessage.AppendFormat("{0}", Encoding.UTF8.GetString(myReadBuffer, 0, numberOfBytesRead));
+                            }
+                            while (stream.DataAvailable);
+                            Byte[] responseData = Encoding.UTF8.GetBytes("УСПЕШНО!");
+                            stream.Write(responseData, 0, responseData.Length);
+                        }
+                    }
+                    finally
+                    {
+                        stream.Close();
+                        client.Close();
+                    }
+                }
+                catch
+                {
+                    server.Stop();
+                    break;
+                }
+            }
+        }
+    }
+}
